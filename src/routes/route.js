e3069807cf48af4641e269be5e8691f9c4965e8a
GauravDattuPise <GauravDattuPise>
2022-12-14 13:55:51 +0530

    This is the players API & I use first time .gitignore

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,31 +16,6 @@ router.get("/test-api" , function(req, res) {
     res.send("hi FunctionUp")
 })
 
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
 router.post("/test-post", function(req, res) {
     res.send([ 23, 45 , 6])
 })
@@ -51,12 +26,12 @@ router.post("/test-post-2", function(req, res) {
 })
 
 router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
+    let id = req.body.user
+    let pwd= req.body.password
 
-    // console.log( id , pwd)
+    console.log( id , pwd)
 
-    console.log( req.body )
+    // console.log( req.body )
 
     res.send(  { msg: "hi" , status: true }  )
 })
@@ -70,4 +45,67 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+
+ 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+ 
+       //LOGIC WILL COME HERE
+               
+        let obj = req.body
+        let Name = req.body.name
+        //console.log(name)
+       
+        // for(let i = 0; i<players.length; i++){
+        //     if(players[i].name.includes(Name)){
+        //         res.send("Name is already exists")
+        //      }
+        //      else if(i === players.length - 1){
+
+        //         players.push(obj)
+        //         res.send({data : players , status : true})
+        //      }
+        //     }        
+
+        let exist = players.find(a => a.name === Name)
+        if(exist){
+            return res.send("Name already exists in array")
+        }
+        else{
+            players.push(obj);
+            res.send({data : players, status : true})
+        }
+      
+   })
+  
 module.exports = router;
