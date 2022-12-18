791b62095584eb397660ececdd6b6f1ee3ed4a67
GauravDattuPise <GauravDattuPise>
2022-12-18 20:08:32 +0530

    This is six api problem sunday

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,33 @@
+// Create a books collection in your DB ( using bookModel with following fields)- bookName( mandatory field), price containing Indian and european price, year ( should be 2021 if no year is provided) , tags array, authorName, totalPages , stockAvailable ( true false)
+// create the following API’s (write logic in bookController and routes in routes):
+// createBook : to create a new entry..use this api to create 11+ entries in your collection
+
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+    bookName: {
+        type : String ,
+        required : true
+    },
+     authorName: String, 
+     tags: [String],
     
-    isPublished: Boolean,
+     isPublished: Boolean,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    year :{ 
+        type : Number,
+         default : 2021
+        },
+    sales: {type: Number, default: 10},
+    totalPages : Number ,
+    stockAvailable : Boolean
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book', bookSchema) // books
 
 //Validation:
 //require:true
